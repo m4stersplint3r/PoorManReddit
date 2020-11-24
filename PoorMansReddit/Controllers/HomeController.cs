2c058a53c57f0844c53562fdcf1ccf202b204b4b
m4stersplint3r <m4stersplint3r@gmail.com>
2020-11-24 01:01:40 -0500

    first commit

---
+++ PoorMansReddit/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PoorMansReddit.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoorMansReddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public RedditDAL DAL = new RedditDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page(string sub)
+        {
+            Rootobject r = DAL.ConvertToPage(sub);
+            if (r.data == null || r.data.after == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(r);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PoorMansReddit/Models/RedditDAL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PoorMansReddit.Models
+{
+    public class RedditDAL
+    {
+        public string GetPage(string sub)
+        {
+            string endpoint = $"https://www.reddit.com/r/{sub}/.json?limit=100";
+            string output;
+
+            HttpWebRequest request = WebRequest.CreateHttp(endpoint);
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader rd = new StreamReader(response.GetResponseStream());
+                output = rd.ReadToEnd();
+            }
+            catch (Exception)
+            {
+                output = "No such sub";
+            }
+            return output;
+        }
+
+        public Rootobject ConvertToPage(string sub)
+        {
+            string pageData = GetPage(sub);
+            if(pageData == "No such sub")
+            {
+                return new Rootobject();
+            }
+            Rootobject r = JsonConvert.DeserializeObject<Rootobject>(pageData);
+
+            return r;
+        }
+        
+    }
+}
